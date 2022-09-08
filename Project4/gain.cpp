150d7d0849b95191b82385305b3f5494c6c6c9fe
youngwoo <eoduq6464@naver.com>
2022-09-08 18:26:20 +0900

    .

---
+++ Project4/gain.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ifstream fin("gain.inp");
+	ofstream fout("gain.out");
+	int layer;
+	fin >> layer;
+
+	int cnt = 0;
+
+	while (cnt < layer) {
+		int size;
+		fin >> size;
+		vector <int> v;
+		int a;
+		for (int i = 0; i < size; i++) {
+			fin >> a;
+			v.push_back(a);
+		}
+		sort(v.begin(), v.end());
+
+		int max1 = v.back();
+		v.pop_back();
+		int max2 = v.back();
+		fout << max1 + max2 - (v[0] + v[1]) << endl;
+		cnt++;
+		v.clear();
+	}
+	fin.close();
+	fout.close();
+}
