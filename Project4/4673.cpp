3da2abda3e351b37827bacf5a3cae66985f1be6e
youngwoo <eoduq6464@naver.com>
2022-09-21 18:14:25 +0900

    ..

--- Project4/4673.cpp
+++ Project4/4673.cpp
@@ -1,39 +1,31 @@
 #include <iostream>
-#include <vector>
-#define MAX 10000
+
+
 using namespace std;
 
-int main() {
-	vector <int> v{1,3,5,7,9,20,31,42,53,64,75,86,97,113};
-	int n = 113;
-	for (int i = 0; i < v.size(); i++) {
-		cout << v[i] << endl;
+int selfNum(int a) {
+	int sum = a;
+
+	while (a != 0) {
+		sum = sum + (a % 10);
+		a = a / 10;
 	}
-	while (true) {
-		int sum = 0;
-		int num = 0;
-		num = n;
-		if (n > 10000) {
-			break;
+	return sum;
+}
+
+int main() {
+	bool check[10001]{ false }; // bool check array 선언
+
+	for (int i = 1; i < 10001; i++) {
+		int cc = selfNum(i);
+		if (cc < 10001) {
+			check[cc] = true;
 		}
-		if (100 < n && n < 1000) {
-			for (int i = 0; i < 3; i++) {
-				sum += num % 10; // 10으로 나눠서 나머지를 sum에 입력
-				num = num / 10; //맨 뒤에 자리수를 없앤다
-			}
-			n = n + sum;
-			cout << n << endl;
-		}
-		if (n > 1000) {
-			for (int i = 0; i < 4; i++) {
-				sum += num % 10; // 10으로 나눠서 나머지를 sum에 입력
-				num = num / 10; //맨 뒤에 자리수를 없앤다
-			}
-			n = n + sum;
-			if (n > 10000) {
-				break;
-			}
-			cout << n << endl;
+	}
+
+	for (int i = 1; i < 10001; i++) {
+		if (!check[i]) {
+			cout << i << endl;
 		}
 	}
 }
