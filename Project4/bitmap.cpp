490ca1e4d61c79cadf585ea484e8542f490c0fd4
youngwoo <eoduq6464@naver.com>
2022-09-28 17:31:32 +0900

    ..

--- Project4/bitmap.cpp
+++ Project4/bitmap.cpp
@@ -1,187 +1,149 @@
-#pragma warning(disable:4996)
+#define _CRT_SECURE_NO_DEPRECATE
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <cmath>
+#include <iomanip>
+#include <cstring>
+#include <fstream>
+
 using namespace std;
 
+vector<vector<char>> v1;
+vector<vector<char>> v2;
+string out, out2;
 
-char arr[205][205];
-string ans = "";
-int idx = 0;
-//string 시간 초과 날 것 같은데..
-string calcBitmap(int strN, int strM, int endN, int endM) {
-	//n,m이 1,1일때,
-	string temp = "";
-	if (strN == endN && strM == endM) {
-		temp += arr[strN][endM];
-		return temp;
-	}
-	//행이 한 줄 일때, M만 분리.
-	if (strN == endN) {
 
-		int divM = (strM + endM) / 2;
-		string temp1 = calcBitmap(strN, strM, endN, divM);
-		string temp2 = calcBitmap(strN, divM + 1, endN, endM);
-		//두 문자 같다면
-		if (temp1 == temp2 && temp1[0] != 'D') {
-			return temp1;
-		}
-		else {
-			temp += 'D' + temp1 + temp2;
-			return temp;
-		}
-	}
-	else if (strM == endM) {
-		//열이 같다면 N만 분리
-		int divN = (strN + endN) / 2;
-		string temp1 = calcBitmap(strN, strM, divN, endM);
-		string temp2 = calcBitmap(divN + 1, strM, endN, endM);
-		//두 문자 같다면
-		if (temp1 == temp2 && temp1[0] != 'D') {
-			return temp1;
-		}
-		else {
-			temp += 'D';
-			temp += temp1;
-			temp += temp2;
-			return temp;
-		}
-	}
-	else {
-		//둘 다 분리해야 한다면,
-		int divN = (strN + endN) / 2;
-		int divM = (strM + endM) / 2;
-		string temp1 = calcBitmap(strN, strM, divN, divM);
-		string temp2 = calcBitmap(strN, divM + 1, divN, endM);
-		string temp3 = calcBitmap(divN + 1, strM, endN, divM);
-		string temp4 = calcBitmap(divN + 1, divM + 1, endN, endM);
+void ifD(int row_num, int col_num, int row_st, int col_st) {
+    char bitmap_data = cin.get();
+    if (bitmap_data == '\n') {
+        bitmap_data = cin.get();
+    }
 
-		if (temp1 == temp2 && temp2 == temp3 && temp3 == temp4 && temp1[0] != 'D') {
-			//같은 숫자라면,
-			return temp1;
-		}
-		else {
-			temp += 'D';
-			temp += temp1;
-			temp += temp2;
-			temp += temp3;
-			temp += temp4;
-			return temp;
-		}
-	}
+    if (bitmap_data == '1' || bitmap_data == '0') {
+        for (int i = row_st; i < row_st + row_num; i++) {
+            for (int j = col_st; j < col_st + col_num; j++) {
+                v2[i][j] = bitmap_data;
+            }
+        }
+    }
+    else {
+        if (col_num == 1) {
+            ifD((int)ceil(row_num / 2.0), col_num, row_st, col_st);
+            ifD(row_num / 2, col_num, row_st + (int)ceil(row_num / 2.0), col_st);
+        }
+        else if (row_num == 1) {
+            ifD(row_num, (int)ceil(col_num / 2.0), row_st, col_st);
+            ifD(row_num, col_num / 2, row_st, (int)ceil(col_st + col_num / 2.0));
+        }
+        else {
+            ifD((int)ceil(row_num / 2.0), (int)ceil(col_num / 2.0), row_st, col_st);
+            ifD((int)ceil(row_num / 2.0), col_num / 2, row_st, col_st + (int)ceil(col_num / 2.0));
+            ifD(row_num / 2, (int)ceil(col_num / 2.0), row_st + (int)ceil(row_num / 2.0), col_st);
+            ifD(row_num / 2, col_num / 2, row_st + (int)ceil(row_num / 2.0), col_st + (int)ceil(col_num / 2.0));
+        }
+        return;
+    }
+    return;
 }
 
-void calcDatamap(int strN, int strM, int endN, int endM) {
-	if (strN == endN && strM == endM) {
-		arr[strN][strM] = ans[idx++];
-		return;
-	}
-	if (ans[idx] == 'D') {
-		idx++;
 
-		if (strN == endN) {
-			int divM = (strM + endM) / 2;
-			calcDatamap(strN, strM, endN, divM);
-			calcDatamap(strN, divM + 1, endN, endM);
-		}
-		else if (strM == endM) {
-			int divN = (strN + endN) / 2;
-			calcDatamap(strN, strM, divN, endM);
-			calcDatamap(divN + 1, strM, endN, endM);
-		}
-		else {
-			int divN = (strN + endN) / 2;
-			int divM = (strM + endM) / 2;
-			calcDatamap(strN, strM, divN, divM);
-			calcDatamap(strN, divM + 1, divN, endM);
-			calcDatamap(divN + 1, strM, endN, divM);
-			calcDatamap(divN + 1, divM + 1, endN, endM);
-		}
-	}
-	else {
-		if (strN == endN) {
-			int divM = (strM + endM) / 2;
-			calcDatamap(strN, strM, endN, divM);
-			idx--;
-			calcDatamap(strN, divM + 1, endN, endM);
-		}
-		else if (strM == endM) {
-			int divN = (strN + endN) / 2;
-			calcDatamap(strN, strM, divN, endM);
-			idx--;
-			calcDatamap(divN + 1, strM, endN, endM);
+void ifB(int row_num, int col_num, int row_st, int col_st) {
+    char bitmap_data = v1[row_st][col_st];
+    for (int i = row_st; i < row_st + row_num; i++) {
+        for (int j = col_st; j < col_st + col_num; j++) {
+            if (bitmap_data != v1[i][j]) {
+                out += 'D';
 
-		}
-		else {
-			int divN = (strN + endN) / 2;
-			int divM = (strM + endM) / 2;
-			calcDatamap(strN, strM, divN, divM);
-			idx--;
-			calcDatamap(strN, divM + 1, divN, endM);
-			idx--;
-			calcDatamap(divN + 1, strM, endN, divM);
-			idx--;
-			calcDatamap(divN + 1, divM + 1, endN, endM);
-
-		}
-	}
+                if (col_num == 1) {
+                    ifB((int)ceil(row_num / 2.0), col_num, row_st, col_st);
+                    ifB(row_num / 2, col_num, row_st + (int)ceil(row_num / 2.0), col_st);
+                }
+                else if (row_num == 1) {
+                    ifB(row_num, (int)ceil(col_num / 2.0), row_st, col_st);
+                    ifB(row_num, col_num / 2, row_st, (int)ceil(col_st + col_num / 2.0));
+                }
+                else {
+                    ifB((int)ceil(row_num / 2.0), (int)ceil(col_num / 2.0), row_st, col_st);
+                    ifB((int)ceil(row_num / 2.0), col_num / 2, row_st, col_st + (int)ceil(col_num / 2.0));
+                    ifB(row_num / 2, (int)ceil(col_num / 2.0), row_st + (int)ceil(row_num / 2.0), col_st);
+                    ifB(row_num / 2, col_num / 2, row_st + (int)ceil(row_num / 2.0), col_st + (int)ceil(col_num / 2.0));
+                }
+                return;
+            }
+        }
+    }
+    out += bitmap_data;
 }
 
 int main() {
-	//ios_base::sync_with_stdio(false);
-	//cin.tie(NULL); cout.tie(NULL);
+    freopen("bitmap.inp", "r", stdin);
+    freopen("bitmap.out", "w", stdout);
 
-	//freopen("1.inp", "r", stdin);
-	//freopen("bitmap.out", "w", stdout);
+    ifstream fin("test.inp");
+    ofstream fout("test.out");
 
+    char data;
+    while (true) {
+        cin >> data;
+        if (data == '#') {
+            break;
+        }
+        int row, col = 0;
+        cin >> row >> col;
+        cin.ignore();
 
-	while (true) {
-		char type;
-		int N, M;
+        if (data == 'B') {
+            cout << 'D' << " " << row << " " << col << endl;
+            string tmpstring;
+            v1.resize(row, vector<char>(col));
+            while (tmpstring.size() < row * col) {
+                string temp;
+                getline(cin, temp);
+                tmpstring += temp;
+            }
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    v1[i][j] = tmpstring[i * col + j];
+                }
+            }
+            ifB(row, col, 0, 0);
 
-		cin >> type;
-		if (type == '#') return 0;
+            int outLen = out.length();
+            int cnt = 0;
+            for (int i = 0; i < outLen; i++) {
+                if (cnt >= 50) {
+                    cnt = 0;
+                    cout << endl;
+                }
+                cout << out[i];
+                cnt++;
+            }
+            cout << endl;
+        }
+        if (data == 'D') {
+            cout << 'B' << " " << row << " " << col << "\n";
+            v2.resize(row, vector<char>(col));
+            ifD(row, col, 0, 0);
+            int cnt = 0;
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (cnt >= 50) {
+                        cnt = 0;
+                        cout << endl;
+                    }
+                    cout << v2[i][j];
+                    cnt++;
+                }
+            }
+            cout << endl;
+        }
 
-		cin >> N >> M;
-
-		if (type == 'B') {
-			//입력
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					cin >> arr[i][j];
-				}
-			}
-			printf("D %3d %3d\n", N, M);
-			//			cout << "D " << N << " " << M << "\n";
-			string temp = calcBitmap(0, 0, N - 1, M - 1);
-			int idx = 0;
-			for (int i = 0; i < temp.size(); i++) {
-				if (idx == 50) {
-					cout << "\n";
-					idx = 0;
-				}
-				idx++;
-				cout << temp[i];
-			}
-			cout << "\n";
-		}
-		else {
-			//
-			cin >> ans;
-			idx = 0;
-			calcDatamap(0, 0, N - 1, M - 1);
-			printf("B %3d %3d\n", N, M);
-			//			cout << "B " << N << " " << M << "\n";
-			idx = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if (idx == 50) {
-						cout << "\n";
-						idx = 0;
-					}
-					idx++;
-					cout << arr[i][j];
-				}
-			}
-			cout << "\n";
-		}
-	}
+        v1.clear();
+        v2.clear();
+        out = "";
+        out2 = "";
+    }
 }
---
+++ Project4/bitmap_qqq.cpp
@@ -0,0 +1,189 @@
+#pragma warning(disable:4996)
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <iostream>
+using namespace std;
+
+
+char arr[205][205];
+string ans = "";
+int idx = 0;
+//string 시간 초과 날 것 같은데..
+string calcBitmap(int strN, int strM, int endN, int endM) {
+	//n,m이 1,1일때,
+	string temp = "";
+	if (strN == endN && strM == endM) {
+		temp += arr[strN][endM];
+		return temp;
+	}
+	//행이 한 줄 일때, M만 분리.
+	if (strN == endN) {
+
+		int divM = (strM + endM) / 2;
+		string temp1 = calcBitmap(strN, strM, endN, divM);
+		string temp2 = calcBitmap(strN, divM + 1, endN, endM);
+		//두 문자 같다면
+		if (temp1 == temp2 && temp1[0] != 'D') {
+			return temp1;
+		}
+		else {
+			temp += 'D' + temp1 + temp2;
+			return temp;
+		}
+	}
+	else if (strM == endM) {
+		//열이 같다면 N만 분리
+		int divN = (strN + endN) / 2;
+		string temp1 = calcBitmap(strN, strM, divN, endM);
+		string temp2 = calcBitmap(divN + 1, strM, endN, endM);
+		//두 문자 같다면
+		if (temp1 == temp2 && temp1[0] != 'D') {
+			return temp1;
+		}
+		else {
+			temp += 'D';
+			temp += temp1;
+			temp += temp2;
+			return temp;
+		}
+	}
+	else {
+		//둘 다 분리해야 한다면,
+		int divN = (strN + endN) / 2;
+		int divM = (strM + endM) / 2;
+		string temp1 = calcBitmap(strN, strM, divN, divM);
+		string temp2 = calcBitmap(strN, divM + 1, divN, endM);
+		string temp3 = calcBitmap(divN + 1, strM, endN, divM);
+		string temp4 = calcBitmap(divN + 1, divM + 1, endN, endM);
+
+		if (temp1 == temp2 && temp2 == temp3 && temp3 == temp4 && temp1[0] != 'D') {
+			//같은 숫자라면,
+			return temp1;
+		}
+		else {
+			temp += 'D';
+			temp += temp1;
+			temp += temp2;
+			temp += temp3;
+			temp += temp4;
+			return temp;
+		}
+	}
+}
+
+void calcDatamap(int strN, int strM, int endN, int endM) {
+	if (strN == endN && strM == endM) {
+		arr[strN][strM] = ans[idx++];
+		return;
+	}
+	if (ans[idx] == 'D') {
+		idx++;
+
+		if (strN == endN) {
+			int divM = (strM + endM) / 2;
+			calcDatamap(strN, strM, endN, divM);
+			calcDatamap(strN, divM + 1, endN, endM);
+		}
+		else if (strM == endM) {
+			int divN = (strN + endN) / 2;
+			calcDatamap(strN, strM, divN, endM);
+			calcDatamap(divN + 1, strM, endN, endM);
+		}
+		else {
+			int divN = (strN + endN) / 2;
+			int divM = (strM + endM) / 2;
+			calcDatamap(strN, strM, divN, divM);
+			calcDatamap(strN, divM + 1, divN, endM);
+			calcDatamap(divN + 1, strM, endN, divM);
+			calcDatamap(divN + 1, divM + 1, endN, endM);
+		}
+	}
+	else {
+		if (strN == endN) {
+			int divM = (strM + endM) / 2;
+			calcDatamap(strN, strM, endN, divM);
+			idx--;
+			calcDatamap(strN, divM + 1, endN, endM);
+		}
+		else if (strM == endM) {
+			int divN = (strN + endN) / 2;
+			calcDatamap(strN, strM, divN, endM);
+			idx--;
+			calcDatamap(divN + 1, strM, endN, endM);
+
+		}
+		else {
+			int divN = (strN + endN) / 2;
+			int divM = (strM + endM) / 2;
+			calcDatamap(strN, strM, divN, divM);
+			idx--;
+			calcDatamap(strN, divM + 1, divN, endM);
+			idx--;
+			calcDatamap(divN + 1, strM, endN, divM);
+			idx--;
+			calcDatamap(divN + 1, divM + 1, endN, endM);
+
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	freopen("test.inp", "r", stdin);
+	freopen("test.out", "w", stdout);
+
+
+	while (true) {
+		char type;
+		int N, M;
+
+		cin >> type;
+		if (type == '#') return 0;
+
+		cin >> N >> M;
+
+		if (type == 'B') {
+			//입력
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					cin >> arr[i][j];
+				}
+			}
+			printf("D %3d %3d\n", N, M);
+						cout << "D " << N << " " << M << "\n";
+			string temp = calcBitmap(0, 0, N - 1, M - 1);
+			int idx = 0;
+			for (int i = 0; i < temp.size(); i++) {
+				if (idx == 50) {
+					cout << "\n";
+					idx = 0;
+				}
+				idx++;
+				cout << temp[i];
+			}
+			cout << "\n";
+		}
+		else {
+
+			cin >> ans;
+			idx = 0;
+			calcDatamap(0, 0, N - 1, M - 1);
+			printf("B %3d %3d\n", N, M);
+						cout << "B " << N << " " << M << "\n";
+			idx = 0;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					if (idx == 50) {
+						cout << "\n";
+						idx = 0;
+					}
+					idx++;
+					cout << arr[i][j];
+				}
+			}
+			cout << "\n";
+		}
+	}
+}
