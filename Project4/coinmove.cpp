c962e2393ebdef2a478bd46c7fd9f8e2046cf315
youngwoo <eoduq6464@naver.com>
2022-10-04 16:40:14 +0900

    ..

--- Project4/coinmove.cpp
+++ Project4/coinmove.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int array[1000001];
 
 int main() {
-	ifstream fin("C:/Users/이영우/Desktop/sampleData11/test.inp");
+	ifstream fin("C:/Users/이영우/Desktop/sampleData11/2.inp");
 	ofstream fout("C:/Users/이영우/Desktop/sampleData11/test.out");
-	
+
 	int layer;
 	fin >> layer;
 	int cnt = 0;
@@ -17,14 +17,28 @@ int main() {
 	while (cnt < layer) {
 		int p, k, s;
 		fin >> p >> k >> s;
-		cout << p << " " << k << " " << s << endl;
+		//cout << p << " " << k << " " << s << endl;
 
 
 		// ex ) 4 6 37일떄 37을 4로 나누면 9이고, 37 - 9 = 28이되어 4의 배수니까 무조건 -1
 		// 2 5 35가 예외
 		// ee
 
+		int a = k + 1;
+		int qwe = s % a;
+		if (qwe == 0) {
+			cout << "영희 못이김" << endl;
 
+		}
+		else if (qwe != 0) {
+			if (qwe % p== 1){
+				cout << "영희 못이김" <<endl;
+				}
+			else {
+				cout << "영희 이김 " << endl;
+			}
+			////cout << " 영희 이김 " << endl;
+		}
 		cnt++;
 	}
 
