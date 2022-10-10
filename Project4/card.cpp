526ae09aad1c7e74d074a4e721b6433f73d3f41e
youngwoo <eoduq6464@naver.com>
2022-10-10 17:32:35 +0900

    ..

--- Project4/card.cpp
+++ Project4/card.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-	ifstream fin("C:/Users/이영우/Desktop/sampleData13/test.inp");
-	ofstream fout("C:/Users/이영우/Desktop/sampleData13/test.out");
+	ifstream fin("card.inp");
+	ofstream fout("card.out");
 	int layer;
 	fin >> layer;
 	
@@ -19,72 +20,96 @@ int main() {
 			fin >> a;
 			v.push_back(a);
 		}
-		cout << " 받아온 벡터 : " << endl;
-		for (int i = 0; i < size; i++) {
-			cout << v[i] << " ";
-		}
-		cout << endl;
+		//cout << " 받아온 벡터 : " << endl;
+		//for (int i = 0; i < size; i++) {
+		//	cout << v[i] << " ";
+		//}
+		//cout << endl;
 
-		vector<int> max(size, 0);
+		vector<int> max2(size, 0);
 		vector<int> index_check(size, -1);
 
 		for (int i = 0; i < v.size(); i ++) {
 			if (i == 0) {
-				max[i] = v[i];
+				max2[i] = v[i];
 				index_check[i] = 1;
 			}
 			if (i == 1) {
-				max[i] = v[i];
+				max2[i] = v[i];
 				index_check[i] = 1;
 			}
 
 			if (i == 2) {
-				max[i] = v[i];
+				max2[i] = v[i];
 			}
 
 			if (i == 3) {
 				if (v[i] + v[i - 2] >= v[i] + v[i - 3]) {
-					max[i] = v[i] + v[i - 2];
+					max2[i] = v[i] + v[i - 2];
 				}
 				else {
-					max[i] = v[i] + v[i - 3];
+					max2[i] = v[i] + v[i - 3];
 				}
 
 			}
-
-			if (i > 3) {
-				//cout << i+1 << " 개 일때"<< endl;
-				int index = i;
-				int time = i / 2;
-
-				int maxmax = v[i] + v[i-2];
-				//cout << "들어오기 전 max 값 : " << maxmax << endl;
-				int cnt = 0;
-				for (int j = i-2; j >= 0; j--) {
-					if (cnt == 3) {
-						break;
-					}
-					if (v[i] + max[j] >= maxmax) {
-						maxmax = v[i] + max[j];
-					}
-					cnt++;
+			if(i == 4){
+				int maxmax = 0;
+				if (v[i] + max2[i - 2] >= v[i] + max2[i - 3]) {
+					maxmax = v[i] + max2[i - 2];
+				}
+				else {
+					maxmax = v[i] + max2[i - 3];
 				}
 
-				max[i] = maxmax;
+				if (maxmax <= v[i] + max2[i - 4]) {
+					maxmax = v[i] + max2[i - 4];
+				}
+				max2[i] = maxmax;
+			}
+			if (i == 5) {
+				int maxmax = 0;
+				if (v[i] + v[i-2] + max2[i-5] >= v[i] + max2[i - 3]) {
+					maxmax = v[i] + v[i - 2] + max2[i - 5];
+				}
+				else {
+					maxmax = v[i] + max2[i - 3];
+				}
+
+				if (maxmax <= v[i] + max2[i - 4]) {
+					maxmax = v[i] + max2[i - 4];
+				}
+
+				max2[i] = maxmax;
+				//cout << "max 값 : " << maxmax << endl;
 
 			}
-		}
-		cout << "최대값 배열 : " << endl;
-		for (int i = 0; i < size; i++) {
-			cout << max[i] << " ";
-		}
-		cout << endl;
+			if (i >= 6) {
+				int maxmax = 0;
+				if (v[i] + max(v[i - 2] + max2[i - 6], v[i-2] + max2[i - 5]) >= v[i] + max2[i - 3]) {
+					maxmax = v[i] + max(v[i - 2] + max2[i - 6], v[i - 2] + max2[i - 5]);
+				}
+				else {
+					maxmax = v[i] + max2[i - 3];
+				}
 
+				if (maxmax <= v[i] + max2[i - 4]) {
+					maxmax = v[i] + max2[i - 4];
+				}
 
+				max2[i] = maxmax;
+				//cout << "max 값 : " << maxmax << endl;
+
+			}
+
+		}
+		//cout << "최대값 배열 : " << endl;
+		//for (int i = 0; i < size; i++) {
+		//	cout << max2[i] << " ";
+		//}
+		int a = max2.back();
+		fout << a << endl;
 
 	}
-
-
 
 	fin.close();
 	fout.close();
