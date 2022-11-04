16f2eb769e2f23c15a7841a2dc24d312e5576ae3
youngwoo <eoduq6464@naver.com>
2022-11-04 23:36:20 +0900

    ㅠㅠ풀었다..

--- Project4/replacement.cpp
+++ Project4/replacement.cpp
@@ -7,12 +7,13 @@ using namespace std;
 
 int m = 5;
 int layer;
-
+int run_size;
 vector <int> val_v;
 vector <int> buf_v;
 vector <int> frz_v(5, 0);
-vector <int> written_v;
 vector <int> run_v;
+vector <int> real_v;
+vector <int> size_check;
 
 
 int main() {
@@ -41,17 +42,7 @@ int main() {
 
 		reverse(val_v.begin(), val_v.end());
 
-		cout << "현재 val 상태 : ";
-		for (int i = 0; i < val_v.size(); i++) {
-			cout << val_v[i] << " ";
-		}
-		cout << endl;
-
-		cout << "현재  buf 상태 : ";
-		for (int i = 0; i < buf_v.size(); i++) {
-			cout << buf_v[i] << " ";
-		}
-
+		int check = 1;
 		while (!val_v.empty()) {
 			vector <int> cc;
 			int idx = 0;
@@ -68,19 +59,33 @@ int main() {
 					idx = i;
 				}
 			}
+
 			run_v.push_back(min);
 			buf_v[idx] = val_v.back();
 			val_v.pop_back();
+
 			if (min > buf_v[idx]) {
 				frz_v[idx] = 1;
 				int cnt = 0;
 				for (int i = 0; i < frz_v.size(); i++) {
-					if(frz_v[i] == 1) {
+					if (frz_v[i] == 1) {
 						cnt++;
 					}
 				}
 				if (cnt == 5) {
-					break;
+					for (int i = 0; i < frz_v.size(); i++) {
+						frz_v[i] = 0;
+					}
+					// 나가기 전 정렬
+					sort(run_v.begin(), run_v.end());
+					run_size = run_v.size();
+					size_check.push_back(run_size);
+					for (int i = 0; i < run_v.size(); i++) {
+						real_v.push_back(run_v[i]);
+					}
+					check++;
+					run_v.clear();
+					continue;
 				}
 			}
 			else {
@@ -88,11 +93,57 @@ int main() {
 			}
 		}
 
+		// 더이상 받아올 값이 없는 상황 처리.
+		vector <int> last;
+		for (int i = 0; i < buf_v.size(); i++) {
+			if (frz_v[i] == 0) {
+				run_v.push_back(buf_v[i]);
+			}
+			else {
+				last.push_back(buf_v[i]);
+			}
+		}
+		// 나가기 전 정렬
+		sort(run_v.begin(), run_v.end());
 
+		run_size = run_v.size();
+		size_check.push_back(run_size);
+		for (int i = 0; i < run_v.size(); i++) {
+			real_v.push_back(run_v[i]);
+		}
+		run_v.clear();
+		for (int i = 0; i < last.size(); i++) {
+			run_v.push_back(last[i]);
+		}
+		// 나가기 전 정렬
+		sort(run_v.begin(), run_v.end());
+		run_size = run_v.size();
+		size_check.push_back(run_size);
+		for (int i = 0; i < run_v.size(); i++) {
+			real_v.push_back(run_v[i]);
+		}
+		cout << size_check.size() << endl;
+		//size_check 벡터는 초기화 안해줘도됨
+		reverse(real_v.begin(), real_v.end());
 
+		for (int i = 0; i < size_check.size(); i++) {
+			int qq = size_check[i];
+			for (int i = 0; i < qq; i++) {
+				int a = real_v.back();
+				cout << a << " ";
+				real_v.pop_back();
+			}
+			cout << endl;
+		}
+		val_v.clear();
+		buf_v.clear();
+		run_v.clear();
+		real_v.clear();
+		size_check.clear();
+		for (int i = 0; i < frz_v.size(); i++) {
+			frz_v[i] = 0;
+		}
 	}
-
-
 
 	fin.close();
 	fout.close();
