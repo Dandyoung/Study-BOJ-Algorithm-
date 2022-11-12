c89f933b194ac5d9e603912f783a7662bcb26f43
youngwoo <eoduq6464@naver.com>
2022-11-12 18:54:18 +0900

    ..

--- Project4/drive.cpp
+++ Project4/drive.cpp
@@ -1,89 +1,81 @@
-#include <iostream>
+#include <iostream>   
+#include <algorithm>
 #include <fstream>
-#include <vector>
-
-
+#include <cstring>
 using namespace std;
 
-int layer;
-int N, M, G, L;
+int min_dp[101][101][202][2];
+int layer, M, N, L, G;
+int row[101][101];
+int col[101][101];
+
+ifstream fin("drive.inp");
+ofstream fout("drive.out");
+
+
+void dp_solve() {
+    for (int i = 1; i < M; i++) {
+        for (int j = 1; j < N; j++) {
+            int maxturn = 2 * max(M, N);
+            for (int k = 1; k <= maxturn; k++) {
+                min_dp[i][j][k][0] = min(min_dp[i][j - 1][k][0] + row[i][j - 1], min_dp[i][j - 1][k - 1][1] + row[i][j - 1]);//왼쪽에서: min(왼->왼, 위-> 왼)
+                min_dp[i][j][k][1] = min(min_dp[i - 1][j][k][1] + col[i - 1][j], min_dp[i - 1][j][k - 1][0] + col[i - 1][j]);//위에에서: min(위->위, 왼->위)
+            }
+        }
+    }
+
+    int maxturn = 2 * max(M, N);
+    for (int k = 0; k <= maxturn; k++) {
+        if (min_dp[M - 1][N - 1][k][1] <= G || min_dp[M - 1][N - 1][k][0] <= G) {
+            int qq = L * (M - 1 + N - 1) + k;
+            fout << qq << '\n';
+            return;
+        }
+    }
+    fout << -1 << '\n';
+    return;
+}
 
 
 int main() {
-	ifstream fin("C:/Users/이영우/Desktop/sampleData20/test.inp");
-	ofstream fout("C:/Users/이영우/Desktop/sampleData20/test.out");
-	fin >> layer;
-	while (layer--) {
-		fin >> N >> M >> G >> L;
+    fin >> layer;
 
-		vector < vector <int> > min_v(N, vector <int>(M, 0));
-		vector< vector<int>> v;
+    while (layer--) {
+        fin >> M >> N >> L >> G;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N - 1; j++) {
+                fin >> row[i][j];
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			vector <int> v2;
-			for (int j = 0; j < N; j++) {
-				int a;
-				fin >> a;
-				v2.push_back(a);
-			}
-			v.push_back(v2);
-			v2.clear();
-		}
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				cout << v[i][j] << " ";
-			}
-			cout << endl;
-		}
-		min_v[0][1] = v[0][1];
-		
-		for (int i = 1; i < M; i++) {
-			min_v[0][i] = min_v[0][i - 1] + v[0][i - 1];
-		}
+        for (int i = 0; i < M - 1; i++) {
+            for (int j = 0; j < N; j++) {
+                fin >> col[i][j];
+            }
+        }
+        // 값 setting
+        int maxturn = 2 * max(M, N);
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k <= maxturn; k++) {
+                    for (int l = 0; l < 2; l++) {
+                        min_dp[i][j][k][l] = 99999999;
+                    }
+                }
+            }
+        }
+        //cout << "------------초기값 세팅 끝(1)-----------" << endl;
+        min_dp[0][0][0][0] = min_dp[0][0][0][1] = 0;
+        for (int i = 1; i < N; i++) {
+            min_dp[0][i][0][0] = min_dp[0][i - 1][0][0] + row[0][i - 1];
+        }
+        for (int i = 1; i < M; i++) {
+            min_dp[i][0][0][1] = min_dp[i - 1][0][0][1] + col[i - 1][0];
+        }
+        //cout << "------------초기값 세팅 끝(2)-----------" << endl;
+        dp_solve();
+    }
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				cout << min_v[i][j] << " ";
-			}
-			cout << endl;
-		}
-		
-		vector< vector<int>> v3;
-		for (int i = 0; i < M; i++) {
-			vector <int> v4;
-			for (int j = 0; j < M; j++) {
-				int a;
-				fin >> a;
-				v4.push_back(a);
-			}
-			v3.push_back(v4);
-			v4.clear();
-		}
-
-		for (int i = 0; i < N-1; i++) {
-			for (int j = 0; j < M; j++) {
-				cout << v3[i][j] << " ";
-			}
-			cout << endl;
-		}
-
-		for (int i = 1; i < N; i++) {
-			min_v[i][0] = min_v[i-1][0] + v3[i-1][0];
-		}
-
-		cout << "------------초기값 세팅 끝-----------" << endl;
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				cout << min_v[i][j] << " ";
-			}
-			cout << endl;
-		}
-
-
-		
-	}
-
-	fin.close();
-	fout.close();
+    fin.close();
+    fout.close();
 }
